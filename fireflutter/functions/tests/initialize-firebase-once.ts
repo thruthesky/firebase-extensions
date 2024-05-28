adc457512e8c1fac3dea58390ac8c0794cf3e1b2
JaeHo Song <thruthesky@gmail.com>
2024-05-28 22:19:17 +0800

    updating firebase init

---
+++ fireflutter/functions/tests/initialize-firebase-once.ts
@@ -0,0 +1,20 @@
+import * as admin from "firebase-admin";
+
+
+let initialized = false;
+
+export const initializeFirebaseOnce = () => {
+    if (admin.apps.length > 0) {
+        return;
+    }
+
+    if (initialized) {
+        return;
+    }
+
+    initialized = true;
+    admin.initializeApp({
+        databaseURL: "https://withcenter-test-3-default-rtdb.firebaseio.com/",
+    });
+};
+
--- fireflutter/functions/tests/user/user-count.spec.ts
+++ fireflutter/functions/tests/user/user-count.spec.ts
@@ -1,10 +1,16 @@
 import { expect } from "chai";
 import "mocha";
+import * as admin from "firebase-admin";
+import { initializeFirebaseOnce } from "../initialize-firebase-once";
+
+initializeFirebaseOnce();
 
 describe("Count no of users", () => {
     it("Should be bigger than 0", async () => {
-        const expected = 1234;
-        return expect(1234).to.eql(expected);
-    }).timeout(10000);
+        const rtdb = admin.database();
+        const snapshot = await rtdb.ref("users").get();
+        console.log("snapshot.numChildren()", snapshot.numChildren());
+        return expect(snapshot.numChildren()).greaterThan(0);
+    });
 });
 
